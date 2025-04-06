88b69fbf7564ee23b5837251a0985c143b112d84
vloliveira <vitorloliveira96@gmail.com>
2025-04-06 11:53:43 -0300

    tratamento de dados de entrada: ao digitar 00h e 24h

--- desafio.py
+++ desafio.py
@@ -8,29 +8,35 @@ saída: 6:05"""
     *Fazer um if para transformar 24h em 12h
 """
 
-hora1 = int(input("Digite a primeira hora"))
-min1 =  int(input("Digite os minutos"))
+hora1 = int(input("Digite a primeira hora: "))
+min1 =  int(input("Digite os minutos: "))
 
-hora2 = int(input("Digite a segunda hora"))
-min2 =  int(input("Digite os minutos"))
+hora2 = int(input("Digite a segunda hora: "))
+min2 =  int(input("Digite os minutos: "))
+
+if hora1 == 0:
+    hora1 = 24
+elif hora2 == 0:
+    hora2 = 24
 
 somaH = hora1 + hora2
 somaMin = min1 + min2
 
 #Conversão das horas e min pra 12h
+if somaMin >= 60:
+    somaH += 1
+    somaMin -= 60
+    if somaMin >= 60:
+        somaH +=1
+        somaMin = 0
+
 if somaH == 0:
     somaH = 12
 elif somaH > 12 and somaH <= 24:
     somaH -= 12
-else:
+elif somaH > 24:
     somaH -= 24
     if somaH > 12:
         somaH -=12
-
-if somaMin == 0:
-    somaMin = 60
-elif somaMin >= 60:
-    somaH += 1
-    somaMin -= 60
 
 print(f"{somaH}:{somaMin:02d}")
