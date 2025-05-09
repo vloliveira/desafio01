aef00a4138f33fee57c73e493360e5e18671622f
vloliveira <vitorloliveira96@gmail.com>
2025-05-09 17:00:01 -0300

    feat: sistema de inserir, deletar e visualizar lista finalizado

--- desafio03/biblioteca.py
+++ desafio03/biblioteca.py
@@ -1,8 +1,8 @@
 
 def listar (lista):
     if lista:
-        for i in range (1, len(lista)):
-            print(f"Tarefa {i} - ", *lista[i])
+        for i in range (len(lista)):
+            print(f"\nTarefa {i} - ", *lista[i], "\n")
     else:
         print("Lista vazia")
 
@@ -11,5 +11,11 @@ def inserir (lista):
     lista.append(tarefa)
 
 def deletar (lista):
-    numero = int(input("Digite o número da tarefa que deseja deletar: "))
-    lista.remove(numero)
+    print("\n")
+    listar(lista)
+    if lista:
+        numero = int(input("Digite o número da tarefa que deseja deletar: "))
+        if numero >=0 and numero <= len(lista):
+            del lista[numero]
+        else:
+            print("Digite um número válido na lista!")
